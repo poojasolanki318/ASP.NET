16e738c51f03712479a6a091cec05cd570542149
Solanki Pooja <solankipooja5600@gmail.com>
2025-08-03 18:39:54 +0530

    Add files via upload

---
+++ FIRSTController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIRST.Controllers
+{
+    public class FIRSTController : Controller
+    {
+        // GET: FIRST
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            //ViewBag.name = "pooja";
+            int num1 = 20;
+            int num2 = 10;
+            int result = num1 + num2;
+            int sub = num1 - num2;
+            int mul = num1 * num2;
+            int div = num1 / num2;
+
+            ViewBag.Num1 = num1;
+            ViewBag.Num2 = num2;
+            ViewBag.Result = result;
+            ViewBag.Sub = sub;
+            ViewBag.Multiply = mul;
+            ViewBag.Div = div;
+
+
+
+
+            return View();
+        }
+    }
+}
---
+++ SECONDController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIRST.Controllers
+{
+    public class SECONDController : Controller
+    {
+        // GET: SECOND
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int num1, int num2)
+        {
+            ViewBag.Num1 = num1;
+            ViewBag.Num2 = num2;
+            ViewBag.Add = num1 + num2;
+            ViewBag.Sub = num1 - num2;
+            ViewBag.Mul = num1 * num2;
+            ViewBag.Div = num1 / num2;
+
+            return View();
+        }
+    }
+}
